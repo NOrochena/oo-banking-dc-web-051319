fe22502df36f9926ad9134f9d32ad99b9a2dc702
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-16 15:32:53 -0400

    Completed pair challenge

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,36 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status, :last_transaction_amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if !(sender.balance - amount < 0)
+      sender.balance -= self.amount
+      receiver.balance += self.amount
+      self.last_transaction_amount = self.amount
+      self.amount = 0
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      sender.balance += self.last_transaction_amount
+      receiver.balance -= self.last_transaction_amount
+      self.status = "reversed"
+    end
+  end
 end
